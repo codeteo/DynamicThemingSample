7f7ba6d74c97b7e996414f63d09fb1f4a235f774
codeteo <ramones.teo@gmail.com>
2018-04-16 21:38:43 +0300

    Adds BottomBar

--- app/src/main/java/theme/dynamic/dynamictheming/MainActivity.kt
+++ app/src/main/java/theme/dynamic/dynamictheming/MainActivity.kt
@@ -1,12 +1,17 @@
 package theme.dynamic.dynamictheming
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomBar: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomBar = findViewById(R.id.bottom_navigation)
     }
 }
